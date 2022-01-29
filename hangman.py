c944ca9d308bc1a94ca19b1fe4d7dc7f7a462950
kimiekomi <kimiekomi1@gmail.com>
2022-01-29 16:17:16 -0700

    create attempt_status()

--- hangman.py
+++ hangman.py
@@ -4,8 +4,15 @@ import word_bank
 def hangman():
     hidden_word = random_word(word_bank.word_bank)
     # print(hidden_word)
-    initial_state = initial_status(hidden_word)
+    initial_state = word_status(hidden_word)
+    attempt_remaining = attempt_status()
     print(initial_state)
+    print(attempt_remaining)
+    print()
+
+    guess_letter = prompt_user()
+
+    # if guess_letter in 
 
     
     
@@ -18,13 +25,29 @@ def random_word(list):
     return hidden_word
 
 
-def initial_status(word):
+def word_status(word):
     initial_state = ""
 
     for character in word:
        initial_state += "_" 
 
     return initial_state
+
+
+def attempt_status():
+    for i in range(1, 7):
+        if i == 1:
+            return "Attempts remaining: X X X X X X"
+        if i == 2:
+            return "Attempts remaining: X X X X X "
+        if i == 3:
+            return "Attempts remaining: X X X X "
+        if i == 4:
+            return "Attempts remaining: X X X "
+        if i == 5:
+            return "Attempts remaining: X X "
+        if i == 6:
+            return "Attempts remaining: X "
 
 
 def prompt_user():
