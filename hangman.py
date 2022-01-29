1b8b557df2e25a2b740ab033629fe8340330247d
kimiekomi <kimiekomi1@gmail.com>
2022-01-29 16:01:27 -0700

    print initial state

--- hangman.py
+++ hangman.py
@@ -1,9 +1,36 @@
 import random
 import word_bank
 
+def hangman():
+    hidden_word = random_word(word_bank.word_bank)
+    # print(hidden_word)
+    initial_state = initial_status(hidden_word)
+    print(initial_state)
+
+    
+    
+
+
+
 def random_word(list):
     hidden_word = random.choice(list)
 
     return hidden_word
 
-print(random_word(word_bank.word_bank))
+
+def initial_status(word):
+    initial_state = ""
+
+    for character in word:
+       initial_state += "_" 
+
+    return initial_state
+
+
+def prompt_user():
+    guess_letter = input("Guess a letter: ")
+
+    return guess_letter
+
+if __name__ == "__main__":
+    hangman()
