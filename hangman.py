991cc363c58de79a65b1dd3387f116601da16d88
kimiekomi <kimiekomi1@gmail.com>
2022-01-29 17:32:43 -0700

    create letter_compare()

--- hangman.py
+++ hangman.py
@@ -3,19 +3,34 @@ import word_bank
 
 def hangman():
     hidden_word = random_word(word_bank.word_bank)
-    # print(hidden_word)
-    initial_state = word_status(hidden_word)
-    attempt_remaining = attempt_status()
-    print(initial_state)
-    print(attempt_remaining)
+    print(hidden_word)
+    word_state = word_status(hidden_word)
+    attempt_remaining = 6
+    attempt_state = attempt_status(attempt_remaining)
+
+    print(word_state)
+    print(attempt_state)
     print()
 
-    guess_letter = prompt_user()
+    new_state = ""
 
-    # if guess_letter in 
+    for i in range(6):
+        guess_letter = prompt_user()
+
+        if not guess_letter.isalpha():
+            print("Enter a valid letter")
+            continue
+    
+        new_state = letter_compare(hidden_word, guess_letter)
+
+        print(new_state)
+        continue
+
+
 
     
-    
+
+
 
 
 
@@ -26,27 +41,27 @@ def random_word(list):
 
 
 def word_status(word):
-    initial_state = ""
+    word_state = ""
 
     for character in word:
-       initial_state += "_" 
+       word_state += "_" 
 
-    return initial_state
+    return word_state
 
 
-def attempt_status():
-    for i in range(1, 7):
-        if i == 1:
+def attempt_status(attempt_remaining):
+    
+        if attempt_remaining == 6:
             return "Attempts remaining: X X X X X X"
-        if i == 2:
+        if attempt_remaining == 5:
             return "Attempts remaining: X X X X X "
-        if i == 3:
+        if attempt_remaining == 4:
             return "Attempts remaining: X X X X "
-        if i == 4:
+        if attempt_remaining == 3:
             return "Attempts remaining: X X X "
-        if i == 5:
+        if attempt_remaining == 2:
             return "Attempts remaining: X X "
-        if i == 6:
+        if attempt_remaining == 1:
             return "Attempts remaining: X "
 
 
@@ -54,6 +69,17 @@ def prompt_user():
     guess_letter = input("Guess a letter: ")
 
     return guess_letter
+
+
+def letter_compare(string, letter):
+    new_state = ""
+    for i in range(len(string)):
+        if letter in string[i]:
+            new_state += letter.upper()
+        else:
+            new_state += "_"
+
+    return new_state
 
 if __name__ == "__main__":
     hangman()
